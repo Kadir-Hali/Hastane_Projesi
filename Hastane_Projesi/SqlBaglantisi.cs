dbeb39f0c5d6c6d4eacf135e0eccb8a64605afa2
Kadir-Hali <kadirhali07@gmail.com>
2025-01-15 00:03:02 +0300

    14.01.2025

---
+++ Hastane_Projesi/SqlBaglantisi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Hastane_Projesi
+{
+    public class SqlBaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection("Data Source=Kadir;Initial Catalog=HastaneProje;Integrated Security=True;Trust Server Certificate=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
